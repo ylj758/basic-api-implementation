54a39b611be64250cc8e95b7d55039fdc68eafec
ylj <758766951@qq.com>
2020-09-16 15:20:04 +0800

    [ylj]: implement name length not more than 8

--- src/test/java/com/thoughtworks/rslist/api/controller/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/controller/UserControllerTest.java
@@ -46,5 +46,17 @@ class UserControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+    @Test
+    void should_register_name_length_not_more_than_8() throws Exception {
+        UserDto userDto = new UserDto("yulvjun888", "femal", 25, "123@qq.com", "12345678911");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userDtoJson = objectMapper.writeValueAsString(userDto);
+
+        mockMvc.perform(post("/user/register")
+                .content(userDtoJson)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
 
 }
