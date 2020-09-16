b2dad01148cd49e43a7070d474fa634354934343
ylj <758766951@qq.com>
2020-09-16 14:54:06 +0800

    [yulvjun]: merge routing

---
+++ src/main/java/com/thoughtworks/rslist/api/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.rslist.api.controller;
+
+import com.thoughtworks.rslist.api.dto.UserDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private List<UserDto> userDtos = new ArrayList<>();
+
+    @PostMapping("/user/register")
+    public void register(@Valid @RequestBody UserDto userDto){
+        userDtos.add(userDto);
+    }
+}
