a3fbb63ab0c3918c2a1bda4b5f6999aa017d2ba0
ylj <758766951@qq.com>
2020-09-16 15:23:18 +0800

    [ylj]: implement email is valid

--- src/test/java/com/thoughtworks/rslist/api/controller/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/controller/UserControllerTest.java
@@ -117,4 +117,16 @@ class UserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    void should_register_email_is_valid() throws Exception {
+        UserDto userDto = new UserDto("ylj", "female", 25, "", "12345678911");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userDtoJson = objectMapper.writeValueAsString(userDto);
+
+        mockMvc.perform(post("/user/register")
+                .content(userDtoJson)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
 }
