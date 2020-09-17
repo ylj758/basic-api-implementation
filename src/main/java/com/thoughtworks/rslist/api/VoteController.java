20a23897a748628a8ea06bef788cc443e4c5e848
ylj <758766951@qq.com>
2020-09-17 22:21:32 +0800

    [ylj]: implement vote success

--- src/main/java/com/thoughtworks/rslist/api/VoteController.java
+++ src/main/java/com/thoughtworks/rslist/api/VoteController.java
@@ -23,12 +23,6 @@ class VoteController {
     @Autowired
     UserService userService;
 
-    //    接口要求：如果用户剩的票数大于等于voteNum，则能成功给rsEventId对应的热搜事件投票
-//    如果用户剩的票数小于voteNum,则投票失败，返回400
-//    考虑到以后需要查询投票记录的需求（根据userId查询他投过票的所有热搜事件，票数和投票时间，根据rsEventId查询所有给他投过票的用户，票数和投票时间），
-//    创建一个Vote表是一个明智的选择
-//            目前不用考虑给热搜事件列表排序的问题
-
     @PostMapping("/rs/vote/{rsEventId}")
     public ResponseEntity<Object> addRsEvent(@PathVariable Integer rsEventId,
                                              @Valid @RequestBody VoteDto voteDto) {
@@ -37,8 +31,7 @@ class VoteController {
         if(userEntity.getVote() < voteDto.getVoteNum()){
             return ResponseEntity.badRequest().build();
         }
-
-
+        voteService.save(voteDto);
         return ResponseEntity.created(null).build();
     }
 }
--- src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
@@ -9,6 +9,7 @@ import com.thoughtworks.rslist.entity.RsEventEntity;
 import com.thoughtworks.rslist.entity.UserEntity;
 import com.thoughtworks.rslist.service.RsEventService;
 import com.thoughtworks.rslist.service.UserService;
+import com.thoughtworks.rslist.service.VoteService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,7 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.sql.Date;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,24 +37,14 @@ class VoteControllerTest {
     private UserService userService;
     @Autowired
     private RsEventService rsEventService;
+    @Autowired
+    private VoteService voteService;
 
     @BeforeEach
     void setUp() {
         userService.deleteAll();
         rsEventService.deleteAll();
     }
-
-//    request: post /rs/vote/{rsEventId}
-//    request body: {
-//        voteNum: 5,
-//                userId: 1,
-//                voteTime: "current time"
-//    }
-//    接口要求：如果用户剩的票数大于等于voteNum，则能成功给rsEventId对应的热搜事件投票
-//    如果用户剩的票数小于voteNum,则投票失败，返回400
-//    考虑到以后需要查询投票记录的需求（根据userId查询他投过票的所有热搜事件，票数和投票时间，根据rsEventId查询所有给他投过票的用户，票数和投票时间），
-//    创建一个Vote表是一个明智的选择
-//            目前不用考虑给热搜事件列表排序的问题
 
     @Test
     void should_vote_unsuccess_when_left_votenum_not_more_than_votenum() throws Exception {
@@ -101,7 +93,7 @@ class VoteControllerTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
 
-
+        assertEquals(1, voteService.findAll().size());
     }
 
     public UserDto getAConcreteUser(){
